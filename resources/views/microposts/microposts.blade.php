778e3cffc9994e504130a00d52d2ab12b8304995
kotaro.agematsu <agematsu@collabo-sys.jp>
2021-06-28 13:16:34 +0000

    post

--- resources/views/microposts/microposts.blade.php
+++ resources/views/microposts/microposts.blade.php
@@ -20,15 +20,15 @@
                             {!! Form::open(['route' => ['microposts.destroy', $micropost->id], 'method' => 'delete']) !!}
                                 {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-sm']) !!}
                             {!! Form::close() !!}
-                            @if (Auth::user()->is_favorite($micropost->id))
-                                {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-                                    {!! Form::submit('Unfavorite', ['class' => 'btn btn-danger btn-success btn-sm']) !!}
-                                {!! Form::close() !!}
-                            @else
-                                {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
-                                    {!! Form::submit('Favorite', ['class' => 'btn btn-light btn-sm']) !!}
-                                {!! Form::close() !!}
-                            @endif
+                        @endif
+                        @if (Auth::user()->is_favorite($micropost->id))
+                            {!! Form::open(['route' => ['favorites.unfavorite', $micropost->id], 'method' => 'delete']) !!}
+                                {!! Form::submit('Unfavorite', ['class' => 'btn btn-danger btn-success btn-sm']) !!}
+                            {!! Form::close() !!}
+                        @else
+                            {!! Form::open(['route' => ['favorites.favorite', $micropost->id]]) !!}
+                                {!! Form::submit('Favorite', ['class' => 'btn btn-light btn-sm']) !!}
+                            {!! Form::close() !!}
                         @endif
                     </div>
                 </div>
